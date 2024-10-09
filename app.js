ad6873367966f1ef983a4b035597744f98d99dae
Akash Martin <akashkumar.fr@gmail.com>
2024-10-09 05:45:06 +0530

    save

--- app.js
+++ app.js
@@ -4,27 +4,34 @@ const port = 3000
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('development.sqlite3');
 
+try {
+	db.serialize(() => {
+		var table_exists = false
+
+		db.each("SELECT name FROM sqlite_master WHERE type='table' AND name='customer';", (err, row) => {
+			console.log(row);
+			table_exists = true
+		}, function() {
+			console.log('im here now')
+			console.log(table_exists)
+			if (table_exists == false) {
+			    const db2 = new sqlite3.Database('development.sqlite3');
+				db2.run("CREATE TABLE customer ( id INTEGER PRIMARY KEY, name VARCHAR(50) NOT NULL, address VARCHAR(100) NOT NULL, phone VARCHAR(15));");
+				db2.close();
+			}
+		});
+
+	});
+
+	db.close();
+} catch (e) {
+	console.log(e)
+}
+
 app.get('/', (req, res) => {
-
-db.serialize(() => {
-    db.run("CREATE TABLE lorem (info TEXT)");
-
-    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (let i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
-
-    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-        console.log(row.id + ": " + row.info);
-    });
-});
-
-db.close();
-
-  res.send('Hello World 1!')
+	res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
